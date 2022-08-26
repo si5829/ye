ae5681f64385a772fe3301ff62282e912802e1a1
si5829 <111752250+si5829@users.noreply.github.com>
2022-08-26 21:24:27 +0300

    Update library.lua

--- library.lua
+++ library.lua
@@ -338,7 +338,7 @@ do
 				Name = "MenuSectionTitle",
 				Parent = library.pagesContainer,
 				BackgroundTransparency = 1,
-				Position = UDim2.new(0, 8, -0.205, 0),
+				Position = UDim2.new(0, 8, -0.230, 0),
 				Size = UDim2.new(0, 76, 1, 0),
 				ZIndex = 3,
 				Font = Enum.Font.Gotham,
