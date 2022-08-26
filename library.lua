bbbfe95637069514eeff175178f4923095670fe8
si5829 <111752250+si5829@users.noreply.github.com>
2022-08-26 23:28:15 +0300

    Update library.lua

--- library.lua
+++ library.lua
@@ -220,7 +220,7 @@ do
 				Name = "Main",
 				BackgroundTransparency = 1,
 				Position = UDim2.new(0.25, 0, 0.052435593, 0),
-				Size = UDim2.new(0, 511, 0, 428),
+				Size = UDim2.new(0, 511, 0, 511),
 				Image = "rbxassetid://4641149554",
 				ImageColor3 = Color3.fromRGB(23, 31, 41),
 				ScaleType = Enum.ScaleType.Slice,
